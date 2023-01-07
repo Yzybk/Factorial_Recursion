c815b423ffbfeeacf41a533d01b979d0f468ac50
yusuf <yusuf@DESKTOP-4BCDGTC>
2023-01-07 17:54:18 +0300

    Add project files.

---
+++ Factorial_Recursion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_Recursion
+{
+    class problem
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n Recursion : Find the factorial of a given number :");
+            Console.WriteLine("///////////////////////////////////////////////////////");
+
+            Console.Write(" Please Input any positive number: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            long fact = FactorialCalcu(n1);
+            Console.WriteLine(" The factorial of {0} is : {1} ", n1, fact);
+            Console.ReadKey();
+        }
+
+        private static long FactorialCalcu(int n1)
+        {
+            if (n1 == 0)
+            {
+                return 1;
+            }
+            return n1 * FactorialCalcu(n1 - 1);
+        }
+    }
+}
